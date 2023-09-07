4d5777f44310b1f9d678b0fc677b2c7669bd6b5e
natapage <624832@gmail.com>
2023-09-07 13:06:56 +0300

    stepper created

---
+++ src/pages/FormPage.vue
@@ -0,0 +1,71 @@
+<script setup>
+import { ref } from 'vue';
+
+const step = ref(1);
+</script>
+
+<template>
+  <div class="q-pa-md absolute-center">
+    <q-stepper
+      v-model="step"
+      ref="stepper"
+      color="primary"
+      animated
+      class="fixed-step-size"
+    >
+      <q-step
+        :name="1"
+        title="Select campaign settings"
+        icon="settings"
+        :done="step > 1"
+      >
+        For each ad campaign that you create, you can control how much you're
+        willing to spend on clicks and conversions, which networks and
+        geographical locations you want your ads to show on, and more.
+      </q-step>
+
+      <q-step
+        :name="2"
+        title="Create an ad group"
+        caption="Optional"
+        icon="create_new_folder"
+        :done="step > 2"
+      >
+        An ad group contains one or more ads which target a shared set of
+        keywords.
+      </q-step>
+
+      <q-step :name="4" title="Create an ad" icon="add_comment">
+        Try out different ad text to see what brings in the most customers, and
+        learn how to enhance your ads using features like ad extensions. If you
+        run into any problems with your ads, find out how to tell if they're
+        running and how to resolve approval issues.
+      </q-step>
+
+      <template v-slot:navigation>
+        <q-stepper-navigation class="absolute-bottom-right">
+          <q-btn
+            @click="$refs.stepper.next()"
+            color="primary"
+            :label="step === 4 ? 'Finish' : 'Continue'"
+          />
+          <q-btn
+            v-if="step > 1"
+            flat
+            color="primary"
+            @click="$refs.stepper.previous()"
+            label="Back"
+            class="q-ml-sm"
+          />
+        </q-stepper-navigation>
+      </template>
+    </q-stepper>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.fixed-step-size {
+  width: 700px;
+  height: 500px;
+}
+</style>
