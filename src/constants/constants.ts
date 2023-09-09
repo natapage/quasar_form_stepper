28947d6cfc097889056e0259462643ac361fbe1c
natapage <624832@gmail.com>
2023-09-09 09:41:06 +0300

    added input rules

---
+++ src/constants/constants.ts
@@ -0,0 +1,36 @@
+export const optionsListTypes = [
+  'Option1',
+  'Option2',
+  'Option3',
+  'Option4',
+  'Option5',
+];
+export const optionsListAddress = [
+  'Option1',
+  'Option2',
+  'Option3',
+  'Option4',
+  'Option5',
+];
+
+export const optionsListIndusrtry = [
+  'Industry1',
+  'Industry2',
+  'Industry3',
+  'Industry4',
+  'Industry5',
+];
+
+export const stepsMap = {
+  1: ['type', 'address', 'addressLine', 'city', 'zip'],
+  2: [
+    'vat',
+    'industry',
+    'email',
+    'iban',
+    'checkbox1',
+    'checkbox2',
+    'checkbox3',
+  ],
+  3: ['name', 'phone', 'accept'],
+};
--- src/pages/FormPage.vue
+++ src/pages/FormPage.vue
@@ -1,34 +1,27 @@
 <script setup>
-import StepOneForm from 'src/components/StepOneForm.vue';
-import StepTwoForm from 'src/components/StepTwoForm.vue';
-import StepThreeForm from 'src/components/StepThreeForm.vue';
-import StepFourForm from 'src/components/StepFourForm.vue';
-
 import { ref, reactive } from 'vue';
+import {
+  optionsListTypes,
+  optionsListAddress,
+  optionsListIndusrtry,
+  stepsMap,
+} from '../constants/constants';
 
 const step = ref(1);
 
-const formData = reactive({
-  model: '',
-  optionsListAdress: ['Option1', 'Option2', 'Option3', 'Option4', 'Option5'],
-  optionsListTypes: ['Option1', 'Option2', 'Option3', 'Option4', 'Option5'],
-  addressLine1: '',
+const data = reactive({
+  type: '',
+  address: '',
+  addressLine: '',
   city: '',
   zip: null,
   vat: ref(null),
   industry: '',
   email: '',
   iban: null,
-  optionsListIndusrtry: [
-    'Industry1',
-    'Industry2',
-    'Industry3',
-    'Industry4',
-    'Industry5',
-  ],
-  Checkbox1: ref(false),
-  Checkbox2: ref(false),
-  Checkbox3: ref(false),
+  checkbox1: ref(false),
+  checkbox2: ref(false),
+  checkbox3: ref(false),
   name: ref(null),
   phone: ref(null),
   message: ref(null),
@@ -52,7 +45,66 @@ const formData = reactive({
         icon="settings"
         :done="step > 1"
       >
-        <component :is="StepOneForm"></component>
+        <div class="q-pa-md" style="max-width: 70%">
+          <q-form class="q-gutter-md">
+            <div>Buisness adress</div>
+            <q-select
+              dense
+              size="sm"
+              outlined
+              v-model="data.address"
+              :options="optionsListAddress"
+              label="Select an address"
+              :rules="[
+                (val) => (val && val.length > 0) || 'Please select an address',
+              ]"
+            />
+
+            <div>Types</div>
+            <q-select
+              dense
+              size="sm"
+              outlined
+              v-model="data.type"
+              :options="optionsListTypes"
+              label="Select a type"
+              :rules="[
+                (val) => (val && val.length > 0) || 'Please select a type',
+              ]"
+            />
+
+            <div>Address</div>
+            <q-input
+              dense
+              outlined
+              v-model.trim="data.addressLine"
+              label="Address"
+              :rules="[
+                (val) => (val && val.length > 0) || 'Please type an address',
+              ]"
+            />
+            <q-input
+              dense
+              outlined
+              v-model="data.city"
+              label="City"
+              :rules="[
+                (val) => (val && val.length > 0) || 'Please type a city',
+              ]"
+            />
+            <q-input
+              dense
+              outlined
+              v-model.trim="data.zip"
+              label="Zip code"
+              type="number"
+              :rules="[
+                (val) =>
+                  (val && val.length === 6) || 'Please type a 6 digit zip code',
+              ]"
+            />
+          </q-form>
+        </div>
       </q-step>
 
       <q-step
@@ -62,7 +114,66 @@ const formData = reactive({
         icon="create_new_folder"
         :done="step > 2"
       >
-        <component :is="StepTwoForm"></component>
+        <div class="q-pa-md" style="max-width: 70%">
+          <q-form class="q-gutter-md">
+            <div>VAT</div>
+            <q-input
+              type="number"
+              dense
+              outlined
+              v-model.trim="data.vat"
+              label="VAT number"
+              :rules="[
+                (val) =>
+                  (val && val.length >= 8 && val.length <= 12) ||
+                  'Please type a 8-12 digits number',
+              ]"
+            />
+
+            <div>Indusrty</div>
+            <q-select
+              dense
+              size="sm"
+              outlined
+              v-model="data.industry"
+              :options="optionsListIndusrtry"
+              label="Please select your industry"
+            />
+
+            <div>Organization e-meil</div>
+            <q-input
+              dense
+              outlined
+              v-model.trim="data.email"
+              label="e-mail"
+              :rules="[
+                (val) =>
+                  (val &&
+                    val.length > 6 &&
+                    val.includes('@') &&
+                    val.includes('.')) ||
+                  'Please type an email',
+              ]"
+            />
+            <div>IBAN</div>
+            <q-input
+              dense
+              outlined
+              v-model.trim="data.iban"
+              label="IBAN"
+              placeholder="Please enter your IBAN"
+              :rules="[
+                (val) =>
+                  (val && val.length === 12) || 'Please type a 12 chars code',
+              ]"
+            />
+            <div class="row">
+              <q-checkbox v-model="data.checkbox1" label="Something 1" />
+              <q-checkbox v-model="data.checkbox2" label="Something 2" />
+              <q-checkbox v-model="data.checkbox3" label="Something 3" />
+            </div>
+          </q-form>
+        </div>
       </q-step>
 
       <q-step
@@ -72,7 +183,48 @@ const formData = reactive({
         icon="create_new_folder"
         :done="step > 3"
       >
-        <component :is="StepThreeForm"></component>
+        <div class="q-pa-md" style="max-width: 70%">
+          <q-form class="q-gutter-md">
+            <div>User info</div>
+            <q-input
+              dense
+              outlined
+              v-model.trim="data.name"
+              label="Username"
+              :rules="[
+                (val) => (val && val.length > 0) || 'Please type something',
+              ]"
+            />
+
+            <q-input
+              dense
+              outlined
+              v-model.trim="data.phone"
+              label="Phone number"
+              hint="Your phone number"
+              :rules="[
+                (val) =>
+                  /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/.test(
+                    val
+                  ) || 'Invalid phone number',
+              ]"
+            />
+
+            <q-input
+              dense
+              outlined
+              type="textarea"
+              autogrow
+              label="Message"
+              v-model.trim="data.message"
+            />
+
+            <q-toggle
+              v-model="data.accept"
+              label="I accept the license and terms"
+            />
+          </q-form>
+        </div>
       </q-step>
 
       <q-step
@@ -81,7 +233,7 @@ const formData = reactive({
         icon="create_new_folder"
         caption="description"
       >
-        <component :is="StepFourForm"></component>
+        <div>Please check the information</div>
       </q-step>
 
       <template v-slot:navigation>
@@ -97,6 +249,12 @@ const formData = reactive({
           <q-btn
             @click="$refs.stepper.next()"
             color="primary"
+            :disable="
+              stepsMap[step]?.some((el) => {
+                if (step === 4) return true;
+                return !data[el];
+              })
+            "
             :label="step === 4 ? 'Finish' : 'Continue'"
           />
         </q-stepper-navigation>
